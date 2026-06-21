2f94cd4c966c57b03943a2dbc428aaa2ddca51ce
ding_mie <ding_mie@163.com>
2026-06-21 10:15:14 +0800

    102. 二叉树的层序遍历

---
+++ 二叉树/102. 二叉树的层序遍历.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        queue=deque()
+        queue.append(root)
+        res=[]
+
+        while queue:
+            n=len(queue)
+            cur_level=[]
+            for i in range(n):
+                node=queue.popleft()
+                cur_level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(cur_level)
+
+        return res
+
+
+
+
+
